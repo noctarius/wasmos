ef9173f2206a7ad0da7b2d055b6d94faeaaa3303
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-03-07 20:08:58 +0100

    Scaffold per-context regions

--- kernel/memory.c
+++ kernel/memory.c
@@ -6,13 +6,32 @@
 static const boot_info_t *g_boot_info;
 static mm_context_t g_contexts[MM_MAX_CONTEXTS];
 static uint32_t g_context_count;
+static mm_context_t g_root_ctx;
 
 void mm_init(const boot_info_t *boot_info) {
     g_boot_info = boot_info;
     g_context_count = 0;
     serial_write("[mm] init\n");
     pfa_init(boot_info);
-    (void)g_boot_info;
+
+    if (mm_context_init(&g_root_ctx, 0) == 0) {
+        mm_context_alloc_region(&g_root_ctx, 16, MEM_REGION_FLAG_READ | MEM_REGION_FLAG_WRITE, MEM_REGION_WASM_LINEAR);
+        mm_context_alloc_region(&g_root_ctx, 4, MEM_REGION_FLAG_READ | MEM_REGION_FLAG_WRITE, MEM_REGION_STACK);
+        mm_context_alloc_region(&g_root_ctx, 8, MEM_REGION_FLAG_READ | MEM_REGION_FLAG_WRITE, MEM_REGION_HEAP);
+    }
+
+    serial_write("[mm] ctx0 regions=");
+    char buf[21];
+    static const char hex[] = "0123456789ABCDEF";
+    uint64_t value = g_root_ctx.region_count;
+    buf[0] = '0';
+    buf[1] = 'x';
+    for (int i = 0; i < 16; ++i) {
+        buf[2 + i] = hex[(value >> ((15 - i) * 4)) & 0xF];
+    }
+    buf[18] = '\n';
+    buf[19] = '\0';
+    serial_write(buf);
 }
 
 int mm_context_init(mm_context_t *ctx, uint32_t id) {

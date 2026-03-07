3cc2c195d14236ae264ccafaa64900dd287e0d57
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-03-07 21:08:32 +0100

    Add labs stub for WAMR build

--- platform/wasmos/platform_internal.h
+++ platform/wasmos/platform_internal.h
@@ -60,6 +60,7 @@ void qsort(void *base, size_t nmemb, size_t size,
            int (*compar)(const void *, const void *));
 void *bsearch(const void *key, const void *base, size_t nmemb, size_t size,
               int (*compar)(const void *, const void *));
+long labs(long n);
 
 #ifdef __cplusplus
 }

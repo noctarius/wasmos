680d3a712f872e738d683961a010d0d4f5754200
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-03-07 19:35:58 +0100

    Add physical frame allocator scaffold

---
+++ kernel/include/physmem.h
@@ -0,0 +1,10 @@
+#ifndef WASMOS_PHYSMEM_H
+#define WASMOS_PHYSMEM_H
+
+#include <stdint.h>
+#include "boot.h"
+
+void pfa_init(const boot_info_t *boot_info);
+uint64_t pfa_alloc_pages(uint64_t pages);
+
+#endif

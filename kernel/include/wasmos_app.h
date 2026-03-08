65c22145842d62b82cd01e8cacdac3751ed93661
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-03-08 13:16:18 +0100

    Enforce WASMOS-APP endpoint and capability requirements

--- kernel/include/wasmos_app.h
+++ kernel/include/wasmos_app.h
@@ -19,6 +19,21 @@
 #define WASMOS_APP_MEM_HINT_IPC    3u
 #define WASMOS_APP_MEM_HINT_DEVICE 4u
 
+#define WASMOS_APP_MAX_REQUIRED_ENDPOINTS 8u
+#define WASMOS_APP_MAX_CAP_REQUESTS 8u
+
+typedef struct {
+    const uint8_t *name;
+    uint32_t name_len;
+    uint32_t rights;
+} wasmos_app_req_endpoint_t;
+
+typedef struct {
+    const uint8_t *name;
+    uint32_t name_len;
+    uint32_t flags;
+} wasmos_app_cap_request_t;
+
 typedef struct {
     const uint8_t *blob;
     uint32_t blob_size;
@@ -31,7 +46,21 @@ typedef struct {
     uint32_t entry_len;
     uint32_t stack_pages_hint;
     uint32_t heap_pages_hint;
+    uint32_t req_ep_count;
+    wasmos_app_req_endpoint_t req_eps[WASMOS_APP_MAX_REQUIRED_ENDPOINTS];
+    uint32_t cap_count;
+    wasmos_app_cap_request_t caps[WASMOS_APP_MAX_CAP_REQUESTS];
 } wasmos_app_desc_t;
+
+typedef int (*wasmos_app_endpoint_resolver_t)(uint32_t owner_context_id,
+                                              const uint8_t *name,
+                                              uint32_t name_len,
+                                              uint32_t rights,
+                                              uint32_t *out_endpoint);
+typedef int (*wasmos_app_capability_granter_t)(uint32_t owner_context_id,
+                                               const uint8_t *name,
+                                               uint32_t name_len,
+                                               uint32_t flags);
 
 typedef struct {
     wasm_driver_t driver;
@@ -40,11 +69,15 @@ typedef struct {
     uint32_t owner_context_id;
     char name[64];
     char entry[64];
+    uint32_t resolved_ep_count;
+    uint32_t resolved_eps[WASMOS_APP_MAX_REQUIRED_ENDPOINTS];
 } wasmos_app_instance_t;
 
 int wasmos_app_parse(const uint8_t *blob, uint32_t blob_size, wasmos_app_desc_t *out_desc);
 int wasmos_app_start(wasmos_app_instance_t *instance, const wasmos_app_desc_t *desc, uint32_t owner_context_id);
 int wasmos_app_dispatch(wasmos_app_instance_t *instance, const ipc_message_t *request, int32_t *out_value);
 void wasmos_app_stop(wasmos_app_instance_t *instance);
+void wasmos_app_set_policy_hooks(wasmos_app_endpoint_resolver_t endpoint_resolver,
+                                 wasmos_app_capability_granter_t capability_granter);
 
 #endif
